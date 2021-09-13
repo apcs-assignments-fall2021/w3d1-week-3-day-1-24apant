d307f8649b99c0dc6236c0bacdf03fab19f67414
anaypant <35899820+anaypant@users.noreply.github.com>
2021-09-13 11:56:25 -0700

    c

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,7 +5,7 @@ public class MyMain {
     //     divisibleByFour(5) -> false
     public static boolean divisibleByFour(int num) {
         // REPLACE WITH YOUR CODE HERE
-        return false;
+        return (num % 4) == 0;
     }
 
     // This method prints every number from 1 up to and including the input biggest
@@ -15,14 +15,30 @@ public class MyMain {
     // ...
     public static void printNumsUpTo(int biggest) {
         // REPLACE THIS WITH YOUR CODE
+        int numLines = biggest/5;
+        int counter = 1;
+        for(int i=0;i<numLines;i++){
+            for (int j=0;j<5;j++){
+                System.out.print(counter + " ");
+                counter ++;
+            }
+            System.out.println("");
+        }
     }
+
 
     // This method is given an input x, and it adds up the sum of its digits
     // Ex. sumDigits(61) -> 7 because 6 + 1 = 7
     //     sumDigits(123) -> 6 because 1 + 2 + 3 = 6
     public static int sumDigits(int x) {
         // REPLACE THIS WITH YOUR CODE
-        return 0;
+        int sum = 0;
+        while(x > 0){
+            sum += x%10;
+            x/= 10;
+
+        }
+        return sum;
     }
 
     // Bonus: This method returns true if the input num is a "self-divisor",
@@ -36,7 +52,18 @@ public class MyMain {
     // digit is NOT a self-divisor.
     public static boolean isSelfDivisor(int num) {
         // REPLACE THIS WITH YOUR CODE
-        return false;
+        int numc = num;
+        while(num > 0){
+            if ((num % 10 == 0)){
+                return false;
+            }
+            if((numc % (num % 10) != 0)){
+                return false;
+            }
+            num/= 10;
+
+        }
+        return true;
     }
 
     public static void main(String[] args) {
